6525686fd607d8f30335f90e10cd6eab51f610fa
dowsky <vasile.bezer.1@gmail.com>
2025-05-31 09:46:31 +0300

    heroku test with port corrected

--- express-app/index.js
+++ express-app/index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World, I\'m deploying pipelines to Heroku!</h1>')
 })
 
-const PORT = 8080
+const PORT = 80
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
