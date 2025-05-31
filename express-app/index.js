aa95c4dd7eeee33f060c172b841755c9b52a2ab5
dowsky <vasile.bezer.1@gmail.com>
2025-05-31 09:42:02 +0300

    heroku test

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World, I\'m deploying pipelines!</h1>')
+  res.send('<h1>Hello World, I\'m deploying pipelines to Heroku!</h1>')
 })
 
 const PORT = 8080
